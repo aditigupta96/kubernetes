04e38201d88a3dbb913da3a4f16e96b59a89b533
Aditi Gupta <aditigpta@google.com>
2025-09-23 22:09:03 -0700

    refactor(apiextensions-apiserver): Make NonStructuralSchema controller context-aware

--- staging/src/k8s.io/apiextensions-apiserver/pkg/apiserver/apiserver.go
+++ staging/src/k8s.io/apiextensions-apiserver/pkg/apiserver/apiserver.go
@@ -244,7 +244,7 @@ func (c completedConfig) New(delegationTarget genericapiserver.DelegationTarget)
 
 		go namingController.RunWithContext(hookContext)
 		go establishingController.RunWithContext(hookContext)
-		go nonStructuralSchemaController.Run(5, hookContext.Done())
+		go nonStructuralSchemaController.RunWithContext(5, hookContext)
 		go apiApprovalController.Run(5, hookContext.Done())
 		go finalizingController.Run(5, hookContext.Done())
 
